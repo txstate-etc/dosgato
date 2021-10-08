c2eb452ff257cb382fb70331bf2466c7de4e7f3b
Anne Faber <anne.faber@txstate.edu>
2021-10-08 10:34:01 -0500

    reorganize DELETE statements so that the order of deletion is preserved and tables are not deleted before other tables that depend on them

--- src/fixtures.ts
+++ src/fixtures.ts
@@ -8,30 +8,37 @@ export async function fixtures () {
     db.execute('DELETE FROM mutationlog'),
     db.execute('DELETE FROM datarules'),
     db.execute('DELETE FROM data'),
-    db.execute('DELETE FROM datafolders'),
     db.execute('DELETE FROM pagetrees_templates'),
     db.execute('DELETE FROM assetrules'),
     db.execute('DELETE FROM pagerules'),
     db.execute('DELETE FROM sites_templates'),
-    db.execute('DELETE FROM templates'),
     db.execute('DELETE FROM resizes'),
     db.execute('DELETE FROM users_roles'),
     db.execute('DELETE FROM groups_roles'),
-    db.execute('DELETE FROM binaries'),
-    db.execute('DELETE FROM pages'),
     db.execute('DELETE FROM users_groups'),
     db.execute('DELETE FROM siterules'),
-    db.execute('DELETE FROM roles'),
     db.execute('DELETE FROM groups_groups'),
-    db.execute('DELETE FROM groups'),
     db.execute('DELETE FROM assets'),
-    db.execute('DELETE FROM sites_managers'),
-    db.execute('DELETE FROM pagetrees'),
-    db.execute('DELETE FROM sites'),
-    db.execute('DELETE FROM assetfolders'),
-    db.execute('DELETE FROM organizations'),
-    db.execute('DELETE FROM users')
+    db.execute('DELETE FROM sites_managers')
   ])
+  await Promise.all([
+    db.execute('DELETE FROM datafolders'),
+    db.execute('DELETE FROM binaries'),
+    db.execute('DELETE FROM pages'),
+    db.execute('DELETE FROM roles'),
+    db.execute('DELETE FROM groups')
+  ])
+  await Promise.all([
+    db.execute('DELETE FROM templates'),
+    db.execute('DELETE FROM pagetrees')
+  ])
+  await db.execute('DELETE FROM sites')
+  await Promise.all([
+    db.execute('DELETE FROM assetfolders'),
+    db.execute('DELETE FROM organizations')
+  ])
+  await db.execute('DELETE FROM users')
+
   const [su01, su02, su03, ed01, ed02, ed03, ed04, ed05] = await Promise.all([
     db.insert('INSERT INTO users (login, name, email, lastlogin, lastlogout, disabledAt) VALUES ("su01", "Michael Scott", "su01@example.com", null, null, null)'),
     db.insert('INSERT INTO users (login, name, email, lastlogin, lastlogout, disabledAt) VALUES ("su02", "Elizabeth Bennet", "su02@example.com", null, null, null)'),
