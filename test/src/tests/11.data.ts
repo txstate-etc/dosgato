52e77183db2e546cb61325fed6102dbc8b17e925
Anne Faber <anne.faber@txstate.edu>
2021-12-02 13:47:01 -0600

    Add stubs for data and datafolder tests

---
+++ test/src/tests/11.data.ts
@@ -0,0 +1,108 @@
+/* eslint-disable @typescript-eslint/no-unused-expressions */
+import { expect } from 'chai'
+import { query } from '../common'
+
+describe('datafolder', () => {
+  it.skip('should return the user who deleted a datafolder', async () => {
+
+  })
+  it.skip('should return null for the user who deleted a datafolder if that datafolder is not deleted', async () => {
+
+  })
+  it.skip('should return the template for a datafolder', async () => {
+
+  })
+  it.skip('should return the site a datafolder belongs to', async () => {
+
+  })
+  it.skip('should return null for a datafolder\'s owning site if the datafolder is for global data', async () => {
+
+  })
+  it.skip('should return all data entries for a datafolder', async () => {
+
+  })
+  it.skip('should return filtered data entries for a datafolder', async () => {
+
+  })
+})
+
+describe('data', () => {
+  it.skip('should filter data entries by internal ID', async () => {
+
+  })
+  it.skip('should filter data entries by ID (dataId)', async () => {
+
+  })
+  it.skip('should return only global data when the global=true filter is used', async () => {
+
+  })
+  it.skip('should only return data belonging to a site when the global=false filter is used', async () => {
+
+  })
+  it.skip('should filter data entries by folder ID', async () => {
+
+  })
+  it.skip('should filter data entries by folder internal ID', async () => {
+
+  })
+  it.skip('should filter data entries by site ID', async () => {
+
+  })
+  it.skip('should filter data entries by template key', async () => {
+
+  })
+  it.skip('should only return deleted data entries when the deleted=true filter is used', async () => {
+
+  })
+  it.skip('should only return undeleted data entries when the deleted=false filter is used', async () => {
+
+  })
+  it.skip('should return the user who deleted a data entry', async () => {
+
+  })
+  it.skip('should return null for the user who deleted a data entry if the data entry is not deleted', async () => {
+
+  })
+  it.skip('should return the JSON data for a data entry (no parameters)', async () => {
+
+  })
+  it.skip('should return the JSON data for the published version of a data entry', async () => {
+
+  })
+  it.skip('should return the JSON data for a particular version of a data entry', async () => {
+
+  })
+  it.skip('should return a data entry\'s template', async () => {
+
+  })
+  it.skip('should return a data entry\'s parent folder', async () => {
+
+  })
+  it.skip('should return null for a data entry\'s parent folder if the data has no folder', async () => {
+
+  })
+  it.skip('should return the site to which a data entry belongs', async () => {
+
+  })
+  it.skip('should return null for site if a data entry is global and has no owning site', async () => {
+
+  })
+  it.skip('should return whether a data entry is published', async () => {
+
+  })
+  it.skip('should return when a data entry was created', async () => {
+
+  })
+  it.skip('should who created a data entry', async () => {
+
+  })
+  it.skip('should return when a data entry was last modified', async () => {
+
+  })
+  it.skip('should return wo last modified a data entry', async () => {
+
+  })
+  it.skip('should return a list of all versions of a data entry', async () => {
+
+  })
+})
