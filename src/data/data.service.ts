e30ed23736533636deb69e20d33464cd102a4e77
Anne Faber <anne.faber@txstate.edu>
2021-12-07 09:19:36 -0600

    Add templateKeys filter for data.

--- src/data/data.service.ts
+++ src/data/data.service.ts
@@ -2,6 +2,8 @@ import { AuthorizedService } from '@txstate-mws/graphql-server'
 import { OneToManyLoader } from 'dataloader-factory'
 import { Data, DataFilter } from './data.model'
 import { getData } from './data.database'
+import { VersionedService } from '../versionedservice'
+import { unique } from 'txstate-utils'
 
 const dataByFolderInternalIdLoader = new OneToManyLoader({
   fetch: async (folderInternalIds: number[], filter?: DataFilter) => {
@@ -13,6 +15,7 @@ const dataByFolderInternalIdLoader = new OneToManyLoader({
 
 export class DataService extends AuthorizedService {
   async find (filter: DataFilter) {
+    filter = await this.processFilters(filter)
     return await getData(filter)
   }
 
@@ -22,5 +25,17 @@ export class DataService extends AuthorizedService {
 
   async mayView (data: Data): Promise<boolean> {
     return true
+  }
+
+  async processFilters (filter: DataFilter) {
+    if (filter.templateKeys?.length) {
+      const searchRule = { indexName: 'templateKey', in: filter.templateKeys }
+      const dataIds = await this.svc(VersionedService).find([searchRule], 'latest')
+      if (filter.ids?.length) {
+        filter.ids.push(...dataIds)
+        filter.ids = unique(filter.ids)
+      } else filter.ids = dataIds
+    }
+    return filter
   }
 }
--- test/src/tests/11.data.ts
+++ test/src/tests/11.data.ts
@@ -4,48 +4,48 @@ import { query } from '../common'
 
 describe('datafolder', () => {
   it('should return the user who deleted a datafolder', async () => {
-    const resp = await query('{ data(filter: { deleted: true }) { id folder { name deletedBy { id } } } }')
-    const entry = resp.data.find((d: any) => d.folder?.name === 'deletedfolder')
+    const { data } = await query('{ data(filter: { deleted: true }) { id folder { name deletedBy { id } } } }')
+    const entry = data.find((d: any) => d.folder?.name === 'deletedfolder')
     expect(entry.folder.deletedBy.id).to.equal('su03')
   })
   it('should return null for the user who deleted a datafolder if that datafolder is not deleted', async () => {
-    const resp = await query('{ data(filter: { deleted: false }) { id folder { name deletedBy { id } } } }')
-    const site2data = resp.data.filter((d: any) => d.folder?.name === 'site2datafolder')
+    const { data } = await query('{ data(filter: { deleted: false }) { id folder { name deletedBy { id } } } }')
+    const site2data = data.filter((d: any) => d.folder?.name === 'site2datafolder')
     for (const dataEntry of site2data) {
       expect(dataEntry.folder.deletedBy).to.be.null
     }
   })
   it('should return the template for a datafolder', async () => {
-    const resp = await query('{ data(filter: { deleted: false }) { id folder { name template { name } } } }')
-    const site2data = resp.data.filter((d: any) => d.folder?.name === 'site2datafolder')
+    const { data } = await query('{ data(filter: { deleted: false }) { id folder { name template { name } } } }')
+    const site2data = data.filter((d: any) => d.folder?.name === 'site2datafolder')
     for (const dataEntry of site2data) {
       expect(dataEntry.folder.template.name).to.equal('datatemplate1')
     }
   })
   it('should return the site a datafolder belongs to', async () => {
-    const resp = await query('{ data(filter: { deleted: false }) { id folder { name site { name } } } }')
-    const site2data = resp.data.filter((d: any) => d.folder?.name === 'site2datafolder')
+    const { data } = await query('{ data(filter: { deleted: false }) { id folder { name site { name } } } }')
+    const site2data = data.filter((d: any) => d.folder?.name === 'site2datafolder')
     for (const dataEntry of site2data) {
       expect(dataEntry.folder.site.name).to.equal('site2')
     }
   })
   it('should return null for a datafolder\'s owning site if the datafolder is for global data', async () => {
-    const resp = await query('{ data(filter: { deleted: false }) { id folder { name site { name } } } }')
-    const globaldata = resp.data.filter((d: any) => d.folder?.name === 'globaldatafolder')
+    const { data } = await query('{ data(filter: { deleted: false }) { id folder { name site { name } } } }')
+    const globaldata = data.filter((d: any) => d.folder?.name === 'globaldatafolder')
     for (const dataEntry of globaldata) {
       expect(dataEntry.folder.site).to.be.null
     }
   })
   it('should return all data entries for a datafolder', async () => {
-    const resp = await query('{ data(filter: { deleted: false }) { id folder { name data { id data } } } }')
-    const site2data = resp.data.find((d: any) => d.folder?.name === 'site2datafolder')
+    const { data } = await query('{ data(filter: { deleted: false }) { id folder { name data { id data } } } }')
+    const site2data = data.find((d: any) => d.folder?.name === 'site2datafolder')
     const dataEntries = site2data.folder.data
     const colors = dataEntries.map((e: any) => e.data.color)
     expect(colors).to.have.members(['red', 'blue', 'green', 'orange'])
   })
   it('should return filtered data entries for a datafolder', async () => {
-    const resp = await query('{ data(filter: { deleted: false }) { id folder { name data(filter:{ deleted:false }) { id data } } } }')
-    const site2data = resp.data.find((d: any) => d.folder?.name === 'site2datafolder')
+    const { data } = await query('{ data(filter: { deleted: false }) { id folder { name data(filter:{ deleted:false }) { id data } } } }')
+    const site2data = data.find((d: any) => d.folder?.name === 'site2datafolder')
     const dataEntries = site2data.folder.data
     const colors = dataEntries.map((e: any) => e.data.color)
     expect(colors).to.have.members(['red', 'blue', 'green'])
@@ -57,19 +57,19 @@ describe('data', () => {
   it('should filter data entries by ID (dataId)', async () => {
     const idResp = await query('{ data(filter: { deleted: false }) { id } }')
     const ids = idResp.data.map((val: any) => val.id)
-    const resp = await query(`{ data(filter: { ids: ["${ids.slice(0, 3).join('","')}"]}) { id }}`)
-    const returnIds = resp.data.map((val: any) => val.id)
+    const { data } = await query(`{ data(filter: { ids: ["${ids.slice(0, 3).join('","')}"]}) { id }}`)
+    const returnIds = data.map((val: any) => val.id)
     expect(returnIds).to.have.members([...ids.slice(0, 3)])
   })
   it('should return only global data when the global=true filter is used', async () => {
-    const resp = await query('{ data(filter: { global: true }) { id site { name } } }')
-    for (const entry of resp.data) {
+    const { data } = await query('{ data(filter: { global: true }) { id site { name } } }')
+    for (const entry of data) {
       expect(entry.site).to.be.null
     }
   })
   it('should only return data belonging to a site when the global=false filter is used', async () => {
-    const resp = await query('{ data(filter: { global: false }) { id site { name } } }')
-    for (const entry of resp.data) {
+    const { data } = await query('{ data(filter: { global: false }) { id site { name } } }')
+    for (const entry of data) {
       expect(entry.site).to.not.be.null
     }
   })
@@ -77,47 +77,51 @@ describe('data', () => {
     const nondeleted = await query('{ data(filter: { deleted:false }) {id folder { id name } } }')
     const site2data = nondeleted.data.find((d: any) => d.folder?.name === 'site2datafolder')
     const folderId = site2data.folder.id
-    const resp = await query(`{ data(filter: { folderIds: ["${folderId}"]}) { id } }`)
-    expect(resp.data).to.have.lengthOf(4)
+    const { data } = await query(`{ data(filter: { folderIds: ["${folderId}"]}) { id } }`)
+    expect(data).to.have.lengthOf(4)
   })
   it('should filter data entries by site ID', async () => {
     const siteResp = await query('{ sites { id name } }')
     const site2Id = siteResp.sites.find((s: any) => s.name === 'site2')
-    const resp = await query(`{ data(filter: { siteIds:["${site2Id}"] }) { id site { name } } }`)
-    for (const entry of resp.data) {
+    const { data } = await query(`{ data(filter: { siteIds:["${site2Id}"] }) { id site { name } } }`)
+    for (const entry of data) {
       expect(entry.site.name).to.equal('site2')
     }
   })
-  it.skip('should filter data entries by template key', async () => {
-
+  it('should filter data entries by template key', async () => {
+    const { data } = await query('{ data(filter: { templateKeys: ["keyd1"] }) { template { key name } data } }')
+    for (const entry of data) {
+      expect(entry.template.key).to.equal('keyd1')
+      expect(entry.data).to.have.property('color')
+    }
   })
   it('should return only deleted data entries when the deleted=true filter is used', async () => {
-    const resp = await query('{ data(filter: { deleted:true }) {id deleted } }')
-    for (const entry of resp.data) {
+    const { data } = await query('{ data(filter: { deleted:true }) {id deleted } }')
+    for (const entry of data) {
       expect(entry.deleted).to.be.true
     }
   })
   it('should return only undeleted data entries when the deleted=false filter is used', async () => {
-    const resp = await query('{ data(filter: { deleted:false }) {id deleted } }')
-    for (const entry of resp.data) {
+    const { data } = await query('{ data(filter: { deleted:false }) {id deleted } }')
+    for (const entry of data) {
       expect(entry.deleted).to.be.false
     }
   })
   it('should return the user who deleted a data entry', async () => {
-    const resp = await query('{ data(filter: { deleted:true }) {id deletedBy { id } } }')
-    for (const entry of resp.data) {
+    const { data } = await query('{ data(filter: { deleted:true }) {id deletedBy { id } } }')
+    for (const entry of data) {
       expect(entry.deletedBy).to.not.be.null
     }
   })
   it('should return null for the user who deleted a data entry if the data entry is not deleted', async () => {
-    const resp = await query('{ data(filter: { deleted:false }) {id deletedBy { id } } }')
-    for (const entry of resp.data) {
+    const { data } = await query('{ data(filter: { deleted:false }) {id deletedBy { id } } }')
+    for (const entry of data) {
       expect(entry.deletedBy).to.be.null
     }
   })
   it('should return the JSON data for a data entry (no parameters)', async () => {
-    const resp = await query('{ data(filter: { deleted:false }) { id data site { name } } }')
-    const site2entries = resp.data.filter((d: any) => d.site?.name === 'site2')
+    const { data } = await query('{ data(filter: { deleted:false }) { id data site { name } } }')
+    const site2entries = data.filter((d: any) => d.site?.name === 'site2')
     expect(site2entries.map((e: any) => e.data.color)).to.have.members(['red', 'blue', 'green'])
   })
   it.skip('should return the JSON data for the published version of a data entry', async () => {
@@ -127,41 +131,41 @@ describe('data', () => {
 
   })
   it('should return a data entry\'s template', async () => {
-    const resp = await query('{ data(filter: { deleted:false }) {id data template { name } site { name } } }')
-    const site2entries = resp.data.filter((d: any) => d.site?.name === 'site2')
+    const { data } = await query('{ data(filter: { deleted:false }) {id data template { name } site { name } } }')
+    const site2entries = data.filter((d: any) => d.site?.name === 'site2')
     for (const entry of site2entries) {
       expect(entry.template.name).to.equal('datatemplate1')
     }
   })
   it('should return a data entry\'s parent folder', async () => {
-    const resp = await query('{ data(filter: { deleted:false }) {id data folder { name } site { name } } }')
-    const site2entries = resp.data.filter((d: any) => d.site?.name === 'site2')
+    const { data } = await query('{ data(filter: { deleted:false }) {id data folder { name } site { name } } }')
+    const site2entries = data.filter((d: any) => d.site?.name === 'site2')
     for (const entry of site2entries) {
       expect(entry.folder.name).to.equal('site2datafolder')
     }
   })
   it('should return null for a data entry\'s parent folder if the data has no folder', async () => {
-    const resp = await query('{ data(filter: { deleted:false }) {id data folder { name } site { name } } }')
-    const filtered = resp.data.filter((d: any) => typeof d.data.name !== 'undefined')
+    const { data } = await query('{ data(filter: { deleted:false }) {id data folder { name } site { name } } }')
+    const filtered = data.filter((d: any) => typeof d.data.name !== 'undefined')
     for (const entry of filtered) {
       expect(entry.folder).to.be.null
     }
   })
   it('should return the site to which a data entry belongs', async () => {
-    const resp = await query('{ data(filter: { global:false }) {id  site { name } } }')
-    for (const entry of resp.data) {
+    const { data } = await query('{ data(filter: { global:false }) {id  site { name } } }')
+    for (const entry of data) {
       expect(entry.site).to.not.be.null
     }
   })
   it('should return null for site if a data entry is global and has no owning site', async () => {
-    const resp = await query('{ data(filter: { global:true }) {id  site { name } } }')
-    for (const entry of resp.data) {
+    const { data } = await query('{ data(filter: { global:true }) {id  site { name } } }')
+    for (const entry of data) {
       expect(entry.site).to.be.null
     }
   })
   it('should return whether a data entry is published', async () => {
-    const resp = await query('{ data(filter: { global:true }) { id data published template { name } } }')
-    const articleData = resp.data.filter((d: any) => d.template.name === 'articledata')
+    const { data } = await query('{ data(filter: { global:true }) { id data published template { name } } }')
+    const articleData = data.filter((d: any) => d.template.name === 'articledata')
     for (const entry of articleData) {
       if (entry.data.title === '5 Steps to a Cleaner Car') {
         expect(entry.published).to.be.true
@@ -171,36 +175,36 @@ describe('data', () => {
     }
   })
   it('should return when a data entry was created', async () => {
-    const resp = await query('{ data(filter: { deleted:false }) {id createdAt } }')
-    for (const entry of resp.data) {
+    const { data } = await query('{ data(filter: { deleted:false }) {id createdAt } }')
+    for (const entry of data) {
       expect(entry.createdAt).to.not.be.null
     }
   })
   it('should who created a data entry', async () => {
-    const resp = await query('{ data(filter: { deleted:false }) {id data createdBy { id } } }')
-    for (const entry of resp.data) {
+    const { data } = await query('{ data(filter: { deleted:false }) {id data createdBy { id } } }')
+    for (const entry of data) {
       expect(entry.createdBy).to.not.be.null
     }
-    const sampleEntry = resp.data.find((d: any) => d.data.color === 'blue')
+    const sampleEntry = data.find((d: any) => d.data.color === 'blue')
     expect(sampleEntry.createdBy.id).to.equal('su01')
   })
   it('should return when a data entry was last modified', async () => {
-    const resp = await query('{ data(filter: { deleted:false }) {id modifiedAt } }')
-    for (const entry of resp.data) {
+    const { data } = await query('{ data(filter: { deleted:false }) {id modifiedAt } }')
+    for (const entry of data) {
       expect(entry.modifiedAt).to.not.be.null
     }
   })
   it('should return who last modified a data entry', async () => {
-    const resp = await query('{ data(filter: { deleted:false }) {id data modifiedBy { id } } }')
-    for (const entry of resp.data) {
+    const { data } = await query('{ data(filter: { deleted:false }) {id data modifiedBy { id } } }')
+    for (const entry of data) {
       expect(entry.modifiedBy).to.not.be.null
     }
-    const sampleEntry = resp.data.find((d: any) => d.data.color === 'blue')
+    const sampleEntry = data.find((d: any) => d.data.color === 'blue')
     expect(sampleEntry.modifiedBy.id).to.equal('su01')
   })
   it('should return a list of all versions of a data entry', async () => {
-    const resp = await query('{ data(filter: { deleted:false }) { id data versions { user { id } version data } } }')
-    const entryWithVersions = resp.data.find((d: any) => d.data.color === 'red')
+    const { data } = await query('{ data(filter: { deleted:false }) { id data versions { user { id } version data } } }')
+    const entryWithVersions = data.find((d: any) => d.data.color === 'red')
     expect(entryWithVersions.versions).to.have.lengthOf(2)
     for (const version of entryWithVersions.versions) {
       if (version.version === 1) {
