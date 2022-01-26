14ab197a5d74898f5896286ae38d316fc7ec3e4c
Anne Faber <anne.faber@txstate.edu>
2022-01-26 11:30:06 -0600

    Add the ability to run tests as different test users

--- test/src/common.ts
+++ test/src/common.ts
@@ -1,20 +1,25 @@
 import axios from 'axios'
 import jwt from 'jsonwebtoken'
 
-const token = jwt.sign({ login: 'su01' }, process.env.JWT_SECRET ?? '')
-
 const client = axios.create({
-  baseURL: 'http://dosgato-api',
-  headers: {
-    Authorization: `Bearer ${token}`
-  }
+  baseURL: 'http://dosgato-api'
 })
 
 export async function query (query: string, variables?: any) {
+  return await queryAs('su01', query, variables)
+}
+
+const tokenCache: Record<string, string> = {}
+export async function queryAs (login: string, query: string, variables?: any) {
+  tokenCache[login] ??= jwt.sign({ login: login }, process.env.JWT_SECRET ?? '')
   try {
     const resp = await client.post('graphql', {
       query,
       ...(variables ? { variables } : {})
+    }, {
+      headers: {
+        authorization: `Bearer ${tokenCache[login]}`
+      }
     })
     if (resp.data.errors?.length) throw new Error(resp.data.errors[0].message)
     return resp.data.data
