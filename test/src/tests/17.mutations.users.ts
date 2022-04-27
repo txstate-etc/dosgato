1cedcf6d9f0d135e6035c254088b3a530b7e072d
Anne Faber <anne.faber@txstate.edu>
2022-04-27 16:07:30 -0500

    fix disable user mutation tests

--- test/src/tests/17.mutations.users.ts
+++ test/src/tests/17.mutations.users.ts
@@ -27,17 +27,17 @@ describe('users mutations', () => {
     await expect(queryAs('ed07', 'mutation UpdateUser ($id: String!, $input: UpdateUserInput!) { updateUser (userId: $id, args: $input) { success user { id name } } }', { id: 'ed10', input: { name: 'Updated Username' } })).to.be.rejected
   })
   it('should disable a user', async () => {
-    const { disableUser: { success } } = await query('mutation DisableUser ($id: String!) { disableUser(userId: $id) { success user { id name } } }', { id: 'ed10' })
+    const { disableUsers: { success } } = await query('mutation DisableUsers ($id: [String]!) { disableUsers(userIds: $id) { success users { id name } } }', { id: ['ed10'] })
     expect(success).to.be.true
     const { users } = await query('{ users(filter: { ids: ["ed10"]}) { id name disabled } }')
     expect(users).to.deep.include({ id: 'ed10', name: 'Updated Username', disabled: true })
   })
   it('should not disable a non-existent user', async () => {
-    await expect(query('mutation DisableUser ($id: String!) { disableUser(userId: $id) { success user { id name } } }', { id: 'fakeuser' })).to.be.rejected
+    await expect(query('mutation DisableUsers ($id: [String]!) { disableUsers(userIds: $id) { success users { id name } } }', { id: ['fakeuser'] })).to.be.rejected
     const { users } = await query('{ users(filter: { enabled: false }) { name } }')
     expect(users).to.not.deep.include({ id: 'fakeuser' })
   })
   it('should not allow an unauthorized user to disable a user', async () => {
-    await expect(queryAs('ed07', 'mutation DisableUser ($id: String!) { disableUser(userId: $id) { success user { id name } } }', { id: 'su01' })).to.be.rejected
+    await expect(queryAs('ed07', 'mutation DisableUsers ($id: [String]!) { disableUsers(userIds: $id) { success users { id name } } }', { id: ['su01'] })).to.be.rejected
   })
 })
