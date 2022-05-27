5480446fe4dd6f17c20947190e8a794294fda8a8
Nick Wing <nwing@txstate.edu>
2022-05-26 20:51:24 -0500

    add id to dataroot model and implement a filter for them

--- src/dataroot/dataroot.model.ts
+++ src/dataroot/dataroot.model.ts
@@ -3,6 +3,9 @@ import { Site, Template } from 'internal'
 
 @ObjectType({ description: 'Each site has an array of dataroots, one per registered templateKey. It contains the data and datafolders of that template type in the site. There is also a set of dataroots for global data.' })
 export class DataRoot {
+  @Field(type => ID)
+  id: string
+
   @Field(type => Site, { nullable: true })
   site?: Site
 
@@ -10,6 +13,7 @@ export class DataRoot {
   template: Template
 
   constructor (site: Site|undefined, template: Template) {
+    this.id = `${site?.id ?? 'global'}-${template.id}`
     this.site = site
     this.template = template
   }
@@ -17,6 +21,9 @@ export class DataRoot {
 
 @InputType()
 export class DataRootFilter {
+  @Field(type => [ID], { nullable: true })
+  ids?: string[]
+
   @Field(type => [ID], { nullable: true, description: 'Return dataroots designated for data of one of the given templates.' })
   templateKeys?: string[]
 
--- src/dataroot/dataroot.service.ts
+++ src/dataroot/dataroot.service.ts
@@ -1,5 +1,5 @@
 import { DataFolder, DataRoot, DosGatoService, Site, SiteService, SiteServiceInternal, Template, TemplateService, TemplateType } from 'internal'
-import { intersect, isNotNull } from 'txstate-utils'
+import { intersect, isNotNull, keyby } from 'txstate-utils'
 import { DataRootFilter } from './dataroot.model'
 
 export class DataRootService extends DosGatoService<DataRoot> {
@@ -8,17 +8,25 @@ export class DataRootService extends DosGatoService<DataRoot> {
   allSites?: Site[]
 
   async find (filter?: DataRootFilter) {
+    const siteService = this.svc(SiteServiceInternal)
     this.templatesById ??= (await this.svc(TemplateService).find({ types: [TemplateType.DATA] })).reduce((map, template) => map.set(template.id, template), new Map())
     this.templatesByKey ??= Array.from(this.templatesById.values()).reduce((map, template) => map.set(template.key, template), new Map())
     filter ??= {}
+    const idPairs = (filter.ids ?? []).map(id => {
+      const [siteId, templateId] = id.split('-')
+      return { siteId: siteId === 'global' ? undefined : siteId, templateId: Number(templateId) }
+    })
+    filter.templateIds = intersect({ skipEmpty: true }, filter.templateIds, idPairs.map(p => p.templateId))
     filter.templateKeys = intersect({ skipEmpty: true }, filter.templateKeys, filter.templateIds?.map(tid => this.templatesById!.get(tid)?.key).filter(isNotNull))
+    filter.siteIds = intersect({ skipEmpty: true }, filter.siteIds, idPairs.map(p => p.siteId).filter(isNotNull))
+
     const templates = filter.templateKeys.length
       ? filter.templateKeys.map(k => this.templatesByKey!.get(k)).filter(isNotNull)
       : Array.from(this.templatesById.values())
-    const dataRoots: DataRoot[] = []
-    if (filter.global !== false) dataRoots.push(...templates.map(t => new DataRoot(undefined, t)))
+    let pairs: { siteId?: string, templateId: number }[] = []
+    if (filter.global !== false) pairs.push(...templates.map(t => ({ templateId: t.id })))
+    let sitesById: Record<string, Site> = {}
     if (filter.global !== true) {
-      const siteService = this.svc(SiteServiceInternal)
       let sites: Site[]
       if (filter.siteIds?.length) {
         sites = (await Promise.all(filter.siteIds.map(async id => await siteService.findById(id)))).filter(isNotNull)
@@ -26,8 +34,12 @@ export class DataRootService extends DosGatoService<DataRoot> {
         this.allSites ??= await this.svc(SiteServiceInternal).find()
         sites = this.allSites
       }
-      for (const site of sites) dataRoots.push(...templates.map(t => new DataRoot(site, t)))
+      sitesById = keyby(sites, 'id')
+      for (const site of sites) pairs.push(...templates.map(t => ({ siteId: site.id, templateId: t.id })))
     }
+    pairs = intersect({ skipEmpty: true }, pairs, idPairs)
+    const filledPairs = pairs.map(p => ({ site: sitesById[p.siteId ?? ''], template: this.templatesById!.get(p.templateId) }))
+    const dataRoots = filledPairs.filter(p => p.template).map(p => new DataRoot(p.site, p.template!))
     return await this.removeUnauthorized(dataRoots)
   }
 
