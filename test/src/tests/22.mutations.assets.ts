7753ec60f67be0a509f2338c318f6fd7f37c289a
Anne Faber <anne.faber@txstate.edu>
2022-09-22 08:56:47 -0500

    Fix assetfolder tests

--- test/src/tests/22.mutations.assets.ts
+++ test/src/tests/22.mutations.assets.ts
@@ -6,8 +6,8 @@ import { DateTime } from 'luxon'
 
 chai.use(chaiAsPromised)
 
-async function createAssetFolder (name: string, siteId: string, parentId: string, username?: string) {
-  const { createAssetFolder: { success, messages, assetFolder } } = await queryAs((username ?? 'su01'), 'mutation CreateAssetFolder ($args: CreateAssetFolderInput!) { createAssetFolder (args: $args) { success messages { message } assetFolder { id name folder { id name } } } }', { args: { siteId, name, parentId } })
+async function createAssetFolder (name: string, parentId: string, username?: string) {
+  const { createAssetFolder: { success, messages, assetFolder } } = await queryAs((username ?? 'su01'), 'mutation CreateAssetFolder ($args: CreateAssetFolderInput!) { createAssetFolder (args: $args) { success messages { message } assetFolder { id name folder { id name } } } }', { args: { name, parentId } })
   return { success, messages, assetFolder }
 }
 
@@ -20,15 +20,15 @@ describe('asset mutations', () => {
     siteAAssetRootId = site.assetroot.id
   })
   it('should create an asset folder', async () => {
-    const { success, assetFolder } = await createAssetFolder('childfolder1', testSiteAId, siteAAssetRootId)
+    const { success, assetFolder } = await createAssetFolder('childfolder1', siteAAssetRootId)
     expect(success).to.be.true
     expect(assetFolder.name).to.equal('childfolder1')
   })
   it('should not allow an unauthorized user to create an asset folder', async () => {
-    await expect(createAssetFolder('test', testSiteAId, siteAAssetRootId, 'ed07')).to.be.rejected
+    await expect(createAssetFolder('test', siteAAssetRootId, 'ed07')).to.be.rejected
   })
   it('should rename an asset folder', async () => {
-    const { assetFolder: folder } = await createAssetFolder('childfolder2', testSiteAId, siteAAssetRootId)
+    const { assetFolder: folder } = await createAssetFolder('childfolder2', siteAAssetRootId)
     const { renameAssetFolder: { success, assetFolder } } = await query('mutation RenameAssetFolder ($folderId: ID!, $name: String!) { renameAssetFolder (folderId: $folderId, name: $name) { success assetFolder { id name } } }', { folderId: folder.id, name: 'childfolder2renamed' })
     expect(success).to.be.true
     expect(assetFolder.name).to.equal('childfolder2renamed')
@@ -37,11 +37,11 @@ describe('asset mutations', () => {
     await expect(query('mutation RenameAssetFolder ($folderId: ID!, $name: String!) { renameAssetFolder (folderId: $folderId, name: $name) { success assetFolder { id name } } }', { folderId: siteAAssetRootId, name: 'shouldnotwork' })).to.be.rejected
   })
   it('should not allow an unauthorized user to rename an asset folder', async () => {
-    const { assetFolder: folder } = await createAssetFolder('childfolder3', testSiteAId, siteAAssetRootId)
+    const { assetFolder: folder } = await createAssetFolder('childfolder3', siteAAssetRootId)
     await expect(queryAs('ed07', 'mutation RenameAssetFolder ($folderId: ID!, $name: String!) { renameAssetFolder (folderId: $folderId, name: $name) { success assetFolder { id name } } }', { folderId: folder.id, name: 'shouldnotwork' })).to.be.rejected
   })
   it('should delete an asset folder', async () => {
-    const { assetFolder: folder } = await createAssetFolder('childfolder4', testSiteAId, siteAAssetRootId)
+    const { assetFolder: folder } = await createAssetFolder('childfolder4', siteAAssetRootId)
     const { deleteAssetFolder: { success, assetFolder } } = await query('mutation DeleteAssetFolder ($folderId: ID!) { deleteAssetFolder (folderId: $folderId) { success assetFolder { id name deleted deletedAt deletedBy { id } } } }', { folderId: folder.id })
     expect(success).to.be.true
     expect(assetFolder.deleted).to.be.true
@@ -52,11 +52,11 @@ describe('asset mutations', () => {
     await expect(query('mutation DeleteAssetFolder ($folderId: ID!) { deleteAssetFolder (folderId: $folderId) { success assetFolder { id name } } }', { folderId: siteAAssetRootId })).to.be.rejected
   })
   it('should not allow an unauthorized user to delete an asset folder', async () => {
-    const { assetFolder: folder } = await createAssetFolder('childfolder5', testSiteAId, siteAAssetRootId)
+    const { assetFolder: folder } = await createAssetFolder('childfolder5', siteAAssetRootId)
     await expect(queryAs('ed07', 'mutation DeleteAssetFolder ($folderId: ID!) { deleteAssetFolder (folderId: $folderId) { success assetFolder { id name deleted deletedAt deletedBy { id } } } }', { folderId: folder.id })).to.be.rejected
   })
   it('should undelete an asset folder', async () => {
-    const { assetFolder: folder } = await createAssetFolder('childfolder6', testSiteAId, siteAAssetRootId)
+    const { assetFolder: folder } = await createAssetFolder('childfolder6', siteAAssetRootId)
     await query('mutation DeleteAssetFolder ($folderId: ID!) { deleteAssetFolder (folderId: $folderId) { success assetFolder { id name deleted deletedAt deletedBy { id } } } }', { folderId: folder.id })
     const { undeleteAssetFolder: { success, assetFolder } } = await query('mutation UndeleteAssetFolder ($folderId: ID!) { undeleteAssetFolder (folderId: $folderId) { success assetFolder { id name deleted deletedAt deletedBy { id } } } }', { folderId: folder.id })
     expect(success).to.be.true
@@ -65,13 +65,13 @@ describe('asset mutations', () => {
     expect(assetFolder.deletedAt).to.be.null
   })
   it('should not allow an unauthorized user to undelete an asset folder', async () => {
-    const { assetFolder: folder } = await createAssetFolder('childfolder7', testSiteAId, siteAAssetRootId)
+    const { assetFolder: folder } = await createAssetFolder('childfolder7', siteAAssetRootId)
     await query('mutation DeleteAssetFolder ($folderId: ID!) { deleteAssetFolder (folderId: $folderId) { success assetFolder { id name deleted deletedAt deletedBy { id } } } }', { folderId: folder.id })
     await expect(queryAs('ed07', 'mutation UndeleteAssetFolder ($folderId: ID!) { undeleteAssetFolder (folderId: $folderId) { success assetFolder { id name deleted deletedAt deletedBy { id } } } }', { folderId: folder.id })).to.be.rejected
   })
   it('should move an asset folder', async () => {
-    const { assetFolder: targetFolder } = await createAssetFolder('childfolder8', testSiteAId, siteAAssetRootId)
-    const { assetFolder: folder } = await createAssetFolder('grandchildfolder1', testSiteAId, siteAAssetRootId)
+    const { assetFolder: targetFolder } = await createAssetFolder('childfolder8', siteAAssetRootId)
+    const { assetFolder: folder } = await createAssetFolder('grandchildfolder1', siteAAssetRootId)
     expect(folder.folder.name).to.equal('assetTestSiteA')
     const { moveAssetFolder: { success, assetFolder } } = await query('mutation MoveAssetFolder ($folderId: ID!, $targetId: ID!) { moveAssetFolder (folderId: $folderId, targetId: $targetId) { success assetFolder { id name folder { id name } } } }', { folderId: folder.id, targetId: targetFolder.id })
     expect(success).to.be.true
@@ -79,17 +79,17 @@ describe('asset mutations', () => {
   })
   it('should not allow the root asset folder to be moved', async () => {
     const { createSite: { site } } = await query('mutation CreateSite ($name: String!, $data: JsonData!) { createSite (name: $name, data: $data) { success site { id name assetroot { id } } } }', { name: 'assetTestSiteB', data: { templateKey: 'keyp1', savedAtVersion: '20220801120000', title: 'Test Title' } })
-    const { assetFolder: targetFolder } = await createAssetFolder('childfolder9', site.id, site.assetroot.id)
+    const { assetFolder: targetFolder } = await createAssetFolder('childfolder9', site.assetroot.id)
     await expect(query('mutation MoveAssetFolder ($folderId: ID!, $targetId: ID!) { moveAssetFolder (folderId: $folderId, targetId: $targetId) { success assetFolder { id name folder { id name } } } }', { folderId: siteAAssetRootId, targetId: targetFolder.id })).to.be.rejected
   })
   it('should not move an asset folder below itself', async () => {
-    const { assetFolder: movingFolder } = await createAssetFolder('childfolder10', testSiteAId, siteAAssetRootId)
-    const { assetFolder: targetFolder } = await createAssetFolder('childfolder11', testSiteAId, movingFolder.id)
+    const { assetFolder: movingFolder } = await createAssetFolder('childfolder10', siteAAssetRootId)
+    const { assetFolder: targetFolder } = await createAssetFolder('childfolder11', movingFolder.id)
     await expect(query('mutation MoveAssetFolder ($folderId: ID!, $targetId: ID!) { moveAssetFolder (folderId: $folderId, targetId: $targetId) { success assetFolder { id name folder { id name } } } }', { folderId: movingFolder.id, targetId: targetFolder.id })).to.be.rejected
   })
   it('should not allow an unauthorized user to move an asset folder', async () => {
-    const { assetFolder: targetFolder } = await createAssetFolder('childfolder12', testSiteAId, siteAAssetRootId)
-    const { assetFolder: folder } = await createAssetFolder('grandchildfolder2', testSiteAId, siteAAssetRootId)
+    const { assetFolder: targetFolder } = await createAssetFolder('childfolder12', siteAAssetRootId)
+    const { assetFolder: folder } = await createAssetFolder('grandchildfolder2', siteAAssetRootId)
     await expect(queryAs('ed07', 'mutation MoveAssetFolder ($folderId: ID!, $targetId: ID!) { moveAssetFolder (folderId: $folderId, targetId: $targetId) { success assetFolder { id name folder { id name } } } }', { folderId: folder.id, targetId: targetFolder.id })).to.be.rejected
   })
 
