dcd598b450d2bf24ad7523d30ca709505058d1ae
Anne Faber <anne.faber@txstate.edu>
2022-10-06 11:26:39 -0500

    Uncomment code that logs all mutations. Add a cache for looking up user internal IDs by login

--- src/util/mutationlogger.ts
+++ src/util/mutationlogger.ts
@@ -1,10 +1,19 @@
 import db from 'mysql2-async/db'
+import { Cache } from 'txstate-utils'
+
+const internalIdCache = new Cache(async (login: string) => {
+  return await db.getval<number>('SELECT id FROM users WHERE login = ?', [login])
+})
 
 export async function logMutation (queryTime: number, operationName: string, query: string, auth: any, variables: any) {
   if (query.includes('mutation')) {
-    // TODO: we have the login and userId is the internal ID for the user. How can we get that internal ID here,
-    // or does it need to be in the auth object?
-    // await db.insert('INSERT INTO mutationlog (userId, mutation, query, variables) VALUES (?,?,?,?)',
-    //   [auth.login, query, operationName, JSON.stringify(variables)])
+    const userId = await internalIdCache.get(auth.sub)
+    if (userId) {
+      await db.insert('INSERT INTO mutationlog (userId, mutation, query, variables) VALUES (?,?,?,?)',
+        [userId, query, operationName, JSON.stringify(variables)])
+    } else {
+      // TODO: still log the mutation if the user can't be found? What would the userId be?
+      console.error(`logMutation: user ${auth.sub} not found.`)
+    }
   }
 }
