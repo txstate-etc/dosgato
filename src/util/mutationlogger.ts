fbb8c879cfef43883e2c95975a75d43725734398
Anne Faber <anne.faber@txstate.edu>
2022-10-06 14:35:44 -0500

    fix switched fields in mutationlog INSERT statement. Add note about validation mutation problem

--- src/util/mutationlogger.ts
+++ src/util/mutationlogger.ts
@@ -5,11 +5,12 @@ const internalIdCache = new Cache(async (login: string) => {
   return await db.getval<number>('SELECT id FROM users WHERE login = ?', [login])
 })
 
+// TODO: Need to make sure data is actually being mutated, not just validated
 export async function logMutation (queryTime: number, operationName: string, query: string, auth: any, variables: any) {
   if (query.includes('mutation')) {
     const userId = await internalIdCache.get(auth.sub)
     if (userId) {
-      // await db.insert('INSERT INTO mutationlog (userId, mutation, query, variables) VALUES (?,?,?,?)',
+      // await db.insert('INSERT INTO mutationlog (userId, query, mutation, variables) VALUES (?,?,?,?)',
       //   [userId, query, operationName, JSON.stringify(variables)])
     } else {
       // TODO: still log the mutation if the user can't be found? What would the userId be?
