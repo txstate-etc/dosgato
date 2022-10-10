59976dc5e26b672fd5eb26e8b452ed20d19141c4
Nick Wing <nwing@txstate.edu>
2022-10-10 16:06:32 -0500

    make the mutation logger start working

--- src/util/mutationlogger.ts
+++ src/util/mutationlogger.ts
@@ -1,20 +1,12 @@
 import db from 'mysql2-async/db'
-import { Cache } from 'txstate-utils'
+import { stringify } from 'txstate-utils'
 
-const internalIdCache = new Cache(async (login: string) => {
-  return await db.getval<number>('SELECT id FROM users WHERE login = ?', [login])
-})
-
-// TODO: Need to make sure data is actually being mutated, not just validated
+// TODO: do not log mutations that failed validation
 export async function logMutation (queryTime: number, operationName: string, query: string, auth: any, variables: any) {
-  if (query.includes('mutation')) {
-    const userId = await internalIdCache.get(auth.sub)
-    if (userId) {
-      // await db.insert('INSERT INTO mutationlog (userId, query, mutation, variables) VALUES (?,?,?,?)',
-      //   [userId, query, operationName, JSON.stringify(variables)])
-    } else {
-      // TODO: still log the mutation if the user can't be found? What would the userId be?
-      console.error(`logMutation: user ${auth.sub} not found.`)
-    }
+  if (!variables.validateOnly && query.trimStart().startsWith('mutation')) {
+    await db.insert(`
+      INSERT INTO mutationlog (userId, query, mutation, variables)
+      SELECT id, ?, ?, ? FROM users WHERE login=?
+    `, [query, operationName, stringify({ ...variables, data: undefined }), auth.sub])
   }
 }
