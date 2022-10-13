31e20aa3e386ceb5e9079adaded5713e6a1f3396
Nick Wing <nwing@txstate.edu>
2022-10-12 19:13:20 -0500

    fix up the mutationlogger to avoid a crash and avoid making entries for mutations that didn't succesfully alter any data

--- src/util/mutationlogger.ts
+++ src/util/mutationlogger.ts
@@ -2,12 +2,11 @@ import { GraphQLError } from 'graphql'
 import db from 'mysql2-async/db'
 import { stringify } from 'txstate-utils'
 
-// TODO: do not log mutations that failed validation
 export async function logMutation (queryTime: number, operationName: string, query: string, auth: any, variables: any, data: any, errors?: GraphQLError[]) {
-  if (!variables.validateOnly && query.trimStart().startsWith('mutation')) {
+  if (!variables?.validateOnly && query.trimStart().startsWith('mutation') && data.success) {
     await db.insert(`
       INSERT INTO mutationlog (userId, query, mutation, variables)
       SELECT id, ?, ?, ? FROM users WHERE login=?
-    `, [query, operationName, stringify({ ...variables, data: undefined }), auth.sub])
+    `, [query, operationName, stringify({ ...variables, data: 'redacted' }), auth.sub])
   }
 }
