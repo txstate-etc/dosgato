0f048bf497ef931903276b07bfb142c9d8c2ed9e
Nick Wing <nwing@txstate.edu>
2022-10-20 08:30:44 -0500

    validate basic page structure like areas must be arrays and components must have template keys

--- src/page/page.validation.ts
+++ src/page/page.validation.ts
@@ -2,13 +2,17 @@ import { ComponentData, ComponentExtras, PageData, PageExtras } from '@dosgato/t
 import { templateRegistry } from '../internal.js'
 
 async function validateRecurse (extras: ComponentExtras, data: ComponentData, path: string[]) {
+  if (!data.templateKey) throw new Error(`Encountered a component without a templateKey at ${path.join('.')}`)
   const validator = templateRegistry.getComponentTemplate(data.templateKey)?.validate
   const messages = (await validator?.(data, extras)) ?? []
   for (const area of Object.keys(data.areas ?? {})) {
     const areaList = data.areas![area]
+    if (areaList == null) continue
+    if (!Array.isArray(areaList)) throw new Error(`Encountered a non-array in area ${[...path, 'areas', area].join('.')}. That is not valid data.`)
     for (let i = 0; i < areaList.length; i++) {
-      const component = areaList[i]
       const subpath = [...path, 'areas', area, String(i)]
+      const component = areaList[i]
+      if (!component) throw new Error(`Encountered an undefined component at ${subpath.join('.')}`)
       messages.push(...await validateRecurse({ ...extras, path: subpath.join('.') }, component, subpath))
     }
   }
@@ -17,12 +21,16 @@ async function validateRecurse (extras: ComponentExtras, data: ComponentData, pa
 
 export async function validatePage (page: PageData, extras: PageExtras) {
   const tmpl = templateRegistry.getPageTemplate(page.templateKey)
+  if (!tmpl) throw new Error('Page data did not contain a templateKey.')
   const messages = (await tmpl.validate?.(page, extras)) ?? []
   for (const area of Object.keys(page.areas ?? {})) {
     const areaList = page.areas![area]
+    if (areaList == null) continue
+    if (!Array.isArray(areaList)) throw new Error(`Encountered a non-array in area ${['areas', area].join('.')}. That is not valid data.`)
     for (let i = 0; i < areaList.length; i++) {
       const component = areaList[i]
       const path = ['areas', area, String(i)]
+      if (!component) throw new Error(`Encountered an undefined component at ${path.join('.')}`)
       messages.push(...await validateRecurse({ ...extras, page, path: path.join('.') }, component, path))
     }
   }
