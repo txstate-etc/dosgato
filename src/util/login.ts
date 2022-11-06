a5f8784201ac88f4bba2dcf31fbe4722e305cfb1
Nick Wing <nwing@txstate.edu>
2022-11-06 10:35:24 -0600

    fix logged error related to setting a user's last login time

--- src/util/login.ts
+++ src/util/login.ts
@@ -2,7 +2,7 @@ import db from 'mysql2-async/db'
 import { DateTime } from 'luxon'
 
 export async function updateLastLogin (userId: string, tokenIssuedAt: number) {
-  const lastLogin = DateTime.fromSeconds(tokenIssuedAt).toUTC().toFormat('y-LL-dd HH:mm:ss')
+  const lastLogin = DateTime.fromSeconds(tokenIssuedAt).toJSDate()
   await db.update('UPDATE users SET lastLogin = ? WHERE login = ?', [lastLogin, userId])
   return lastLogin
 }
