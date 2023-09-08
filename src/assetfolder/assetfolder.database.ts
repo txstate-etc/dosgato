a1f715ef953d544f65eaabb233acf3fdae657de8
Nick Wing <nwing@txstate.edu>
2023-09-08 10:13:23 -0500

    better fix for deleting assetfolders, over-corrected on the last try, release 1.1.3

--- src/assetfolder/assetfolder.database.ts
+++ src/assetfolder/assetfolder.database.ts
@@ -193,7 +193,7 @@ export async function renameAssetFolder (folderId: string, name: string) {
 
 export async function deleteAssetFolder (id: number, userInternalId: number) {
   await db.transaction(async db => {
-    const folderIds = await db.getvals<number>('SELECT id FROM assetfolders WHERE id = ? OR path like ?', [id, `%/${id}/%`])
+    const folderIds = await db.getvals<number>('SELECT id FROM assetfolders WHERE id = ? OR path like ? OR path like ?', [id, `%/${id}/%`, `%/${id}`])
     const binds: number[] = [userInternalId, DeleteState.MARKEDFORDELETE]
     await db.update(`UPDATE assetfolders SET deletedBy = ?, deletedAt = NOW(), deleteState = ? WHERE id IN (${db.in(binds, folderIds)})`, binds)
     await db.update(`UPDATE assets SET deletedBy = ?, deletedAt = NOW(), deleteState = ? WHERE folderId IN (${db.in(binds, folderIds)})`, binds)
@@ -203,7 +203,7 @@ export async function deleteAssetFolder (id: number, userInternalId: number) {
 export async function finalizeAssetFolderDeletion (id: number, userInternalId: number) {
   const deleteTime = DateTime.now().toFormat('yLLddHHmmss')
   await db.transaction(async db => {
-    const folderIds = await db.getvals<number>('SELECT id FROM assetfolders WHERE id = ? OR path like ?', [id, `%/${id}/%`])
+    const folderIds = await db.getvals<number>('SELECT id FROM assetfolders WHERE id = ? OR path like ? OR path like ?', [id, `%/${id}/%`, `%/${id}`])
     const binds: number[] = [userInternalId, DeleteState.DELETED]
     await db.update(`UPDATE assetfolders SET deletedBy = ?, deletedAt = NOW(), deleteState = ?, name = CONCAT(name, '-${deleteTime}') WHERE id IN (${db.in(binds, folderIds)})`, binds)
     await db.update(`UPDATE assets SET deletedBy = ?, deletedAt = NOW(), deleteState = ? WHERE folderId IN (${db.in([], folderIds)})`, binds)
@@ -211,7 +211,7 @@ export async function finalizeAssetFolderDeletion (id: number, userInternalId: n
 }
 export async function undeleteAssetFolder (id: number) {
   await db.transaction(async db => {
-    const folderIds = await db.getvals<number>('SELECT id FROM assetfolders WHERE id = ? OR path like ?', [id, `%/${id}/%`])
+    const folderIds = await db.getvals<number>('SELECT id FROM assetfolders WHERE id = ? OR path like ? OR path like ?', [id, `%/${id}/%`, `%/${id}`])
     const binds: number[] = [DeleteState.NOTDELETED]
     await db.update(`UPDATE assetfolders SET deletedBy = null, deletedAt = null, deleteState = ? WHERE id IN (${db.in(binds, folderIds)})`, binds)
     await db.update(`UPDATE assets SET deletedBy = null, deletedAt = null, deleteState = ? WHERE folderId IN (${db.in([], folderIds)})`, binds)
